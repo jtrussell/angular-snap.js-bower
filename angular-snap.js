05a87597cc2c471fe46aeb9c36a82f159666a911
Justin <jus.russell@gmail.com>
2014-01-28 09:45:58 -0500

    chore: Only list non-minified files under "main"

--- angular-snap.js
+++ angular-snap.js
@@ -1,5 +1,17 @@
 angular.module('snap', []);
 
+(function() {
+  'use strict';
+  var version = [1, 3, 4]
+    , vObj = {
+        full: version.join('.'),
+        major: version[0],
+        minor: version[1],
+        patch: version[2]
+      };
+  angular.module('snap').constant('SNAP_VERSION', vObj);
+}());
+
 angular.module('snap')
   .directive('snapClose', ['$rootScope', 'snapRemote', function($rootScope, snapRemote) {
     'use strict';
